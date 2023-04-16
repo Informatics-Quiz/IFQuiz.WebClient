d1cffba79e92235a0b92933100125df5d0441bee
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-04-16 20:21:12 +0700

    Fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// eslint-disable-next-line
-import Navbar from './Components/Navbar1/Navbar'
-// eslint-disable-next-line
+import Navbar from './Components/Navbar'
 import Welcome from './Pages/Welcome/Welcome'
-// eslint-disable-next-line
 import Login from './Pages/Login/Login'
-// eslint-disable-next-line
 import Signup from './Pages/Signup/Signup'
-// eslint-disable-next-line
 import Register from './Pages/Register/Register'
-// eslint-disable-next-line
 import LoginEmail from './Pages/LoginEmail/LoginEmail'
-// eslint-disable-next-line
-import Navbar2 from './Components/Navbar2/Navbar2'
-// eslint-disable-next-line
 import CreateQuiz from './Pages/CreateQuiz/CreateQuiz'
-// eslint-disable-next-line
 import Home from './Pages/Home/Home'
-// eslint-disable-next-line
 import EditUserProfile from './Pages/EditUserProfile/EditUserProfile'
 
 import { Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
 
 function App() {
-	const user = useSelector((state) => state.user.authUser)
+	// const user = useSelector((state) => state.user.authUser)
 
 	return (
 		<div className="App">
-			{!user ? <Navbar /> : <Navbar2 />}
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Welcome />} />
 				<Route path="/Login" element={<Login />} />
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,62 @@
+import { FaHome } from 'react-icons/fa'
+import { TbRotateClockwise2 } from 'react-icons/tb'
+import React from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../../Reducers/userRedeucer'
+import { MdLogout } from 'react-icons/md'
+
+const Navbar2 = () => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const isAuthenticated = useSelector((state) => state.user.authUser) === null ? false : true
+
+	function handleClickLogout() {
+		dispatch(setUser(null))
+		navigate('/')
+	}
+
+	return (
+		<nav className="navbar navbar-expand-md" style={{ backgroundColor: '#161B22' }}>
+			{isAuthenticated ? (
+				<div className="container-fluid py-2 px-4">
+					<div className="d-flex align-items-center">
+						<Link
+							to="/Home"
+							className="d-flex align-items-center mx-2"
+							style={{ textDecoration: 'none', color: 'white' }}
+						>
+							<FaHome />
+							<button className="mx-xl-1">Home</button>
+						</Link>
+						<div className="d-flex align-items-center mx-2">
+							<TbRotateClockwise2 />
+							<button className="mx-xl-1">Activity</button>
+						</div>
+					</div>
+					<div className="d-flex align-items-center">
+						<MdLogout />
+						<button onClick={handleClickLogout} className="mx-xl-1">
+							Logout
+						</button>
+					</div>
+				</div>
+			) : (
+				<div className="container-fluid py-2 px-4">
+					<Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+						IF Quiz
+					</Link>
+					<div className="d-flex align-items-center">
+						<Link to="/Login" className="mx-2" style={{ textDecoration: 'none', color: 'white' }}>
+							Login
+						</Link>
+						<Link to="/Signup" className="mx-2" style={{ textDecoration: 'none', color: 'white' }}>
+							Signup
+						</Link>
+					</div>
+				</div>
+			)}
+		</nav>
+	)
+}
+export default Navbar2
