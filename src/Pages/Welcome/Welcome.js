4117df592f45dfa1a48e5000867550f9aadbbcbc
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-12 06:43:46 +0700

    done: public page

--- src/Pages/Welcome/Welcome.js
+++ src/Pages/Welcome/Welcome.js
@@ -1,9 +1,20 @@
 import './Welcome.css'
+import Navbar from '../../Components/Navbar'
+import { ReactComponent as ReactLogo } from '../../logo.svg'
 const Welcome = () => {
     return (
-        <div className='ContainerWelcome'>
-            <div className='Text'>Welcome To IF Quiz</div>
-        </div>
+        <>
+            
+			<Navbar />
+            <div className='welcome__container'>
+                <div className='title__container'>
+                    <ReactLogo className='logo'/>
+                    <div className='title'>INFORMATICS QUIZ</div>
+                </div>
+                <div className='description'>Welcome to the Burapha University Student Quiz and Exam Portal! Are you a Burapha University student looking to enhance your learning experience, test your knowledge, and excel in your exams? You've come to the right place! Our portal is dedicated to providing you with a comprehensive platform for taking quizzes, exams, and test preparation specific to Burapha University's curriculum.</div>
+            </div>
+        </>
+
     )
 }
 export default Welcome
