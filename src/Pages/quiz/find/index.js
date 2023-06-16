545591bf1c5a3941baa7c9bf58a1fef57c0249a9
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-16 18:56:20 +0700

    remove unuse code

--- src/Pages/quiz/find/index.js
+++ src/Pages/quiz/find/index.js
@@ -7,7 +7,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getQuizByCode } from "../../../services/quiz";
 
-import ModalStatus from "../../../components/modals/edit-status";
 import Navbar from "../../../components/navbar";
 import Notify from "../../../components/notify";
 
