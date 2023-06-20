bca34ed6b4c8d4fdb51273e94d368ca22edb210f
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-20 08:40:01 +0700

    change name reducer files to "take"

---
+++ src/reducers/take.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+	quiz: null,
+}
+
+const quizSlice = createSlice({
+	name: 'take',
+	initialState,
+	reducers: {
+		setTakingQuiz: (state, action) => {
+			state.quiz = action.payload
+		},
+		setSelectedQuestionId: (state, action) => {
+			state.quiz.selectedQuestionId = action.payload
+		},
+		setSelectedChoiceId: (state, action) => {
+			state.quiz.answers[state.quiz.selectedQuestionId].selectedId = action.payload
+		},
+		setSelectedMultipleChoice: (state, action) => {
+			const { handler, selectId } = action.payload
+			if (handler === 'select') {
+				state.quiz.answers[state.quiz.selectedQuestionId].selectedIds.push(selectId)
+			} else if (handler === 'unselect') {
+				state.quiz.answers[state.quiz.selectedQuestionId].selectedIds = state.quiz.answers[state.quiz.selectedQuestionId].selectedIds.filter(
+					id => id !== selectId
+				)
+			}
+		}
+	},
+})
+
+export default quizSlice.reducer
+export const { setTakingQuiz, setSelectedQuestionId, setSelectedChoiceId, setSelectedMultipleChoice } = quizSlice.actions
